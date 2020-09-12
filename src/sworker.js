4d99336a4f362dea37be27b1216e5d077cc620c3
srahmed <srehanpk@gmail.com>
2020-09-12 13:27:57 +0500

    Update sworker.js

--- src/sworker.js
+++ src/sworker.js
@@ -1,16 +1,31 @@
 export default function sWorker () {
 
   let sWorker = `${process.env.PUBLIC_URL}/sw.js`;
-  navigator.serviceWorker.register(sWorker).then ((result) => {
 
-    console.log("result", result);
-  })
-  .catch ((err)=> {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register(sWorker).then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 
-    console.log("Err", err);
-  })
 }
 
 
 
 
+
+
+// navigator.serviceWorker.register(sWorker).then ((result) => {
+
+//   console.log("result", result);
+// })
+// .catch ((err)=> {
+
+//   console.log("Err", err);
+// })
